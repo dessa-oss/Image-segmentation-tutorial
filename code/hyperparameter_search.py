f7727e2c1e8709f65690a34e54e231c34bfb96e6
Aydin Göze Polat <mrchesstr@gmail.com>
2019-11-18 18:36:27 -0500

    ap: More diverse running times for preemption

--- code/hyperparameter_search.py
+++ code/hyperparameter_search.py
@@ -2,12 +2,12 @@ import os
 import numpy as np
 import foundations
 
-NUM_JOBS = 10
+NUM_JOBS = 20
 
 def generate_params():
 
     hyper_params = {'batch_size': int(np.random.choice([8, 16, 32, 64])),
-                    'epochs': int(np.random.choice([100, 200])),
+                    'epochs': int(np.random.choice([15, 100])),
                     'learning_rate': np.random.choice([0.1, 0.01, 0.001, 0.0001]),
                     'decoder_neurons': [np.random.randint(16, 512), np.random.randint(16, 512),
                                         np.random.randint(16, 512), np.random.randint(16, 512)],
